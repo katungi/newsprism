1d00cf373fe3c7ae13f9f106251625e151528831
katungi <dankatdennis@gmail.com>
2021-01-31 08:08:20 +0300

    added boilerplate code for authentication

---
+++ utils/auth0.ts
@@ -0,0 +1,21 @@
+import auth0 from '@auth0/nextjs-auth0'
+import getConfig from 'next/config'
+
+const { serverRuntimeConfig } = getConfig();
+
+const { auth, cookieSecret } = serverRuntimeConfig;
+export default initAuth0({
+    ...auth,
+    session: {
+        cookieSecret,
+        cookieLifeTime: 60 * 60 * 8,
+        storeIdToken: false,
+        storeAccessToken: false,
+        storeRefreshToken: false
+    },
+    oidcClient: {
+        httpTimeout: 2500,
+        clockTolerance: 10000
+    }
+});
+ 
